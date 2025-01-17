d7ada8559011ac14b2318ae4be9f1e13fdeffdf4
Iman Mombo <163603192+MomboSauce@users.noreply.github.com>
2025-01-17 20:37:24 +0000

    New changes

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source "https://rubygems.org"
 
-gem "puma", "~> 5.6.7"
-gem "rackup", "~> 1.0.1"
-gem "sinatra", "~> 3.0.6"
+gem "puma"
+gem "rackup"
+gem "sinatra"
+gem "sinatra-contrib"
+gem "better_errors"
+gem "binding_of_caller"
--- dice.rb
+++ dice.rb
@@ -1,9 +1,75 @@
 require "sinatra"
+require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
 
-get("/") do
-  "Hello World"
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
+
+get("/") do 
+  erb(:elephant)
 end
 
 get("/zebra") do
   "We must add a route for each path we want to support"
+end
+
+get("/ddgoose") do
+  "Hopefully this shows up without having to start allova again"
+end
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  erb(:two_six, { :layout => :wrapper })
+end
+
+
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
+end
+
+
+
+get("/dice/1/20") do
+  first_die = rand(1..20)
+  second_die = rand(1..20)
+  sum = first_die
+	
+  outcome = "You rolled a #{first_die} a total of #{sum}."
+	
+  "<h1>1d20</h1>
+   <p>#{outcome}</p>"
+end
+
+
+
+
+get("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  third_die = rand(1..4)
+  fourth_die = rand(1..4)
+  fifth_die = rand(1..4)
+  sum = first_die + second_die + third_die + fourth_die 
+	
+    outcome = "You rolled a #{first_die} and a #{second_die} and a #{third_die} and a #{fourth_die} for a total of #{sum}."
+	
+    "<h1>5d4</h1>
+   <p>#{outcome}</p>"
 end
